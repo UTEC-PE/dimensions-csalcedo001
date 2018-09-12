5e860667e7b4f82fee36f1d6e3997821f47c7651
César Salcedo <cesar.salcedo@utec.edu.pe>
2018-09-11 21:58:57 -0500

    Al tests passed\!

--- main.cpp
+++ main.cpp
@@ -6,11 +6,25 @@
 using namespace std;
 
 template <typename T>
-struct Dimensions {      
+struct Dimensions {
     int operator()(int* coordinates, int* sizes, int dimensions) {
-        // TODO
+        int total = 0;
+
+        for (int i = 0, mult = 1; i < dimensions; i++, mult *= sizes[i])
+            total += coordinates[i] * mult;
+
+        return total;
     }
 };
+
+int f(int* coordinates, int* sizes, int dimensions) {
+    int total = 0;
+
+    for (int i = 0, mult = 1; i < dimensions; i++, mult *= sizes[i])
+        total += coordinates[i] * mult;
+
+    return total;
+}
 
 struct Integer {
        typedef int T;
@@ -18,6 +32,30 @@ struct Integer {
 };
 
 int main(int argc, char *argv[]) {
+    int tuple2[] = {2, 3, 5};
+    int tuple[] = {0, 0, 0};
+
+    Vector <Integer> vector(3, tuple2);
+
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 3; j++)
+            for (int k = 0; k < 2; k++) {
+                *tuple = k;
+                *(tuple + 1) = j;
+                *(tuple + 2) = i;
+
+                vector.set(i + j + k, tuple);
+            }
+
+      for (int i = 0; i < 5; i++)
+          for (int j = 0; j < 3; j++)
+              for (int k = 0; k < 2; k++) {
+                  *tuple = k;
+                  *(tuple + 1) = j;
+                  *(tuple + 2) = i;
+                  cout << *tuple << " + " << *(tuple + 1) << " + " << *(tuple + 2) << " = " << vector.get(tuple) << endl;
+              }
+
     system("pause");
     return EXIT_SUCCESS;
 }
